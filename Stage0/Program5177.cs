878ec62873b7dc051d08dbb1172b81c42c6ea200
shirZP <shir.p999@gmail.com>
2023-12-25 17:37:26 +0200

    another welcome function

---
+++ Stage0/Program5177.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        private static void Welcome5177()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
