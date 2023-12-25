cae22397ae70a83aa07a143c56bb71ed2c3d0c44
YaelIzralevitch <yaeliz150@gmail.com>
2023-12-25 17:16:54 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program9165.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Targil0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome9165();
+            Welcome5177();
+            Console.ReadKey();
+        }
+
+        private static void Welcome9165()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+        static partial void Welcome5177();      
+    }
+}
