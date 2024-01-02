a702390ba60bb9202fff9cf443cf2ba3b11f6bfe
shirZP <shir.p999@gmail.com>
2024-01-02 19:43:56 +0200

    Add Engineer object

---
+++ DalFacade/DO/Engineer.cs
@@ -0,0 +1,22 @@
+﻿namespace DO;
+/// <summary>
+/// An engineer entity represents an engineer with all its properties
+/// </summary>
+/// <param name="Id">The engineer's unique personal identity card (as in a national identity card)</param>
+/// <param name="FullName">Engineer's first and last name</param>
+/// <param name="Email">The email address of the engineer</param>
+/// <param name="Level">The level of expertise of the engineer</param>
+/// <param name="Cost">How much per hour does the engineer get</param>
+public record Engineer
+(
+    int Id,
+    string? FullName,
+    string? Email,
+    DO.EngineerExperience? Level,
+    double? Cost
+)
+{
+    public Engineer() : this(0, "", "", null, 0) { } //empty ctor for stage 3
+    // TODO: parameter ctor
+
+}
