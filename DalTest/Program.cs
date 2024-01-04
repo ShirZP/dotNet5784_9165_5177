9afacf1a7282e045b342297ceee3035faba9ef26
YaelIzralevitch <yaeliz150@gmail.com>
2024-01-04 23:30:06 +0200

    update main

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -3,6 +3,7 @@
     using Dal;
     using DalApi;
     using DO;
+    
     internal class Program
     {
         private static ITask? s_dalTask = new TaskImplementation(); //stage 1
@@ -13,27 +14,70 @@
         {
             Initialization.Do(s_dalTask, s_dalEngineer, s_dalDependency);
 
-            char choice;
-          //  do 
-         //   {
+            int choice;
+            printMainMenu();
+            choice = Console.Read();
+
+
+            do
+            {
                 try
                 {
-                    Console.WriteLine("");
-
-
-
+                    switch (choice)
+                    {
+                        case 1:
+                            taskSubMenu();
+                            break;
+                        case 2:
+                            engineerSubMenu();
+                            break;
+                        case 3:
+                            dependencySubMenu();
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            break;
+                    }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex);
                 }
-               // choice = Console.ReadLine();
-            //}
-           // while (choice != '0' )
+
+                printMainMenu();
+                choice = Console.Read();
+
+            } while (choice != 0);
             
         }
         
-        
+        private static void printMainMenu()
+        {
+            Console.WriteLine("Select an entity of your choice:");
+            Console.WriteLine("1 - Task\n" + "2 - Engineer\n" + "3 - Dependency\n" + "0 - Exit\n");
+        }
+
+        private static void printSubMenu(string entityName)
+        {
+            Console.WriteLine($"Select an action for the {entityName} entity:");
+            Console.WriteLine("1 - Create\n" + "2 - Read\n" + "3 - ReadAll\n" + "4 - Update\n" + "5 - Delete\n" + "0 - Exit\n");
+        }
+
+        private static void taskSubMenu()
+        {
+            printSubMenu("Task"); 
+        }
+
+        private static void engineerSubMenu()
+        {
+            printSubMenu("Engineer");
+        }
+
+        private static void dependencySubMenu()
+        {
+            printSubMenu("Dependency");
+        }
 
     }
 
