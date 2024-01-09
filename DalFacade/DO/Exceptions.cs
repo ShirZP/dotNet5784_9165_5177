dadf9f6040e3e2550e9d1d2010377034a0a161b9
YaelIzralevitch <yaeliz150@gmail.com>
2024-01-09 18:18:49 +0200

    added Exception.cs

---
+++ DalFacade/DO/Exceptions.cs
@@ -0,0 +1,14 @@
+﻿namespace DO;
+
+[Serializable]
+public class DalDoesNotExistException : Exception  //אובייקט לא קיים
+{
+    public DalDoesNotExistException(string? message) : base(message) { }
+}
+
+
+[Serializable]
+public class DalAlreadyExistsException : Exception  //אובייקט כבר קיים
+{
+    public DalAlreadyExistsException(string? message) : base(message) { }
+}
