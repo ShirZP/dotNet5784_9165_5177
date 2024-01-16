8566b0785c1f07b4e5c1ccf157a0847ea7e7beae
YaelIzralevitch <yaeliz150@gmail.com>
2024-01-16 17:35:37 +0200

    changes

--- DalFacade/DalApi/ICrud.cs
+++ DalFacade/DalApi/ICrud.cs
@@ -5,7 +5,7 @@ public interface ICrud<T> where T : class
 {
     int Create(T item); //Creates new entity object in DAL
     T? Read(Func<T, bool> filter); //stage 2 - Reads entity object by filter 
-    IEnumerable<T?> ReadAll(Func<T, bool>? filter = null); // stage 2 - Reads all entities objects by filter 
+    IEnumerable<T?> ReadAll(Func<T, bool>? filter = null); //stage 2 - Reads all entities objects by filter 
     void Update(T item); //Updates entity object
     void Delete(int id); //Deletes an object by its Id
 }
