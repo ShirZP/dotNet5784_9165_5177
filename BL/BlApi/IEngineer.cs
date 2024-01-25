6739aabee7ccd07869685c48ca0bbe331225c38c
YaelIzralevitch <yaeliz150@gmail.com>
2024-01-25 14:21:15 +0200

    added IEngineer & IBl to BlApi

---
+++ BL/BlApi/IEngineer.cs
@@ -0,0 +1,15 @@
+﻿namespace BlApi;
+
+public interface IEngineer
+{
+    //TODO: לשאול האם BO/DO בשליחה
+    public IEnumerable<BO.Engineer> ReadAll(Func<BO.Engineer, bool>? filter = null);  //Reads all engineers by filter
+
+    public BO.Engineer Read(int id); //Reads engineer by id
+
+    public void Create(BO.Engineer engineer); //Add new engineer
+
+    public void Update(BO.Engineer UpdatedEngineer); //Updates engineer
+
+    public void Delete(int id);  //Deletes a engineer by its id
+}
