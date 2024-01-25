5d8b9217540e9747e1ddaa4dc0027d23521bf970
YaelIzralevitch <yaeliz150@gmail.com>
2024-01-25 13:26:54 +0200

    added TaskInEngineer.cs & EngineerInTask.cs to BO

---
+++ BL/BO/TaskInEngineer.cs
@@ -0,0 +1,14 @@
+﻿namespace BO;
+
+public class TaskInEngineer
+{
+    /// <summary>
+    /// ID of the task 
+    /// </summary>
+    public int ID { get; init; }
+
+    /// <summary>
+    /// Nickname of the task
+    /// </summary>
+    public string NickName { get; set; }
+}
