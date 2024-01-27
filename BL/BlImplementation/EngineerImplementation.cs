9a587591908c28828d0f51d05166ba8fdc28c6bf
YaelIzralevitch <yaeliz150@gmail.com>
2024-01-27 21:29:40 +0200

    added EngineerImplementation.cs & MilestoneImplementation.cs to BL

---
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -0,0 +1,35 @@
+﻿namespace BlImplementation;
+using BlApi;
+using BO;   //TODO: delete
+using System;
+using System.Collections.Generic;
+
+internal class EngineerImplementation : IEngineer
+{
+    private DalApi.IDal _dal = Factory.Get;
+
+    public void Create(Engineer engineer)
+    {       
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Engineer Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Engineer> ReadAll(Func<Engineer, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Engineer UpdatedEngineer)
+    {
+        throw new NotImplementedException();
+    }
+}
