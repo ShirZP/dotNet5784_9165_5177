2ef00955ad6cdb764345234149f3be2640a1e913
YaelIzralevitch <yaeliz150@gmail.com>
2024-02-08 13:52:35 +0200

    fix Bl.cs

--- BL/BlApi/IBl.cs
+++ BL/BlApi/IBl.cs
@@ -5,7 +5,7 @@ public interface IBl
     public ITask Task { get; }
     public IEngineer Engineer { get; }
 
-    public BO.ProjectStatus GetProjectStatus();
+    //public BO.ProjectStatus GetProjectStatus();
 
     //public IMilestone Milestone { get; }   //TODO: 
 }
--- BL/BlImplementation/Bl.cs
+++ BL/BlImplementation/Bl.cs
@@ -4,9 +4,8 @@ namespace BlImplementation;
 
 internal class Bl : IBl
 {
-    public 
-
     public ITask Task => new TaskImplementation();
 
     public IEngineer Engineer => new EngineerImplementation();
+    //public BO.ProjectStatus GetProjectStatus();
 }
