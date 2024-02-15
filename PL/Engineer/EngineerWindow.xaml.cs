593560096c9552d5a26619a5d7655fa824aef4ac
YaelIzralevitch <yaeliz150@gmail.com>
2024-02-15 13:57:18 +0200

    update EngineerWindow

--- PL/Engineer/EngineerWindow.xaml.cs
+++ PL/Engineer/EngineerWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,41 @@ namespace PL.Engineer
     /// </summary>
     public partial class EngineerWindow : Window
     {
-        public EngineerWindow()
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+        public static readonly DependencyProperty EngineerProperty = DependencyProperty.Register(
+                                                                                        "CurrentEngineer",
+                                                                                        typeof(BO.Engineer),
+                                                                                        typeof(EngineerWindow),
+                                                                                        new PropertyMetadata(null));
+
+
+        public BO.Engineer CurrentEngineer
+        {
+            get { return (BO.Engineer)GetValue(EngineerProperty); }
+            set { SetValue(EngineerProperty, value); }
+        }
+
+        public EngineerWindow(int id = 0)
         {
             InitializeComponent();
+
+            if (id == 0) 
+            {
+                CurrentEngineer = new BO.Engineer(0, "", "", BO.EngineerExperience.Beginner, null, null);
+            }
+            else
+            {
+                try
+                {
+                    CurrentEngineer = s_bl?.Engineer.Read(id)!;
+                }
+                catch(BO.BlDoesNotExistException ex)
+                { 
+                    MessageBox.Show(ex.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            
         }
     }
 }
