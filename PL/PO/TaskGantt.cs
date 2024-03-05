ce32be2aaad66781ab892e953a0af1bd9cc603d6
YaelIzralevitch <yaeliz150@gmail.com>
2024-03-05 08:56:37 +0200

    add gantt chart

---
+++ PL/PO/TaskGantt.cs
@@ -0,0 +1,11 @@
+﻿namespace PL.PO;
+
+public class TaskGantt
+{
+    public int taskID { get; init; }
+    public string taskName { get; init; }
+    public int duration { get; set; }    //width of rectangle task
+    public int timeFromStart { get; set; }  //width of empty rectangle from start to task
+    public int timeToEnd { get; set; }    //width of empty rectangle from task to end
+}
+
