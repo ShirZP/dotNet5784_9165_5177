1849ebb4823c2bbe4128b954e26f0d2eac8b79db
shirZP <shir.p999@gmail.com>
2024-04-02 21:33:46 +0300

    update PL

--- DalFacade/DalApi/IUser.cs
+++ DalFacade/DalApi/IUser.cs
@@ -7,4 +7,5 @@ public interface IUser
     User? Read(Func<User, bool> filter); //Reads entity object by filter 
     void Delete(int id); //Deletes an object by its Id
     void Clear(); //Clear all the objects from the data
+    string PasswordHash(string str); //Hash the password of the user
 }
